9132189d5c4fe2b5221bce20d250c45965ccf8f2
Rodrigo Brito <rodrigobritocontato@gmail.com>
2025-12-19 15:30:18 -0400

    feat: add temperature conversion utilities

--- assets/utils.js
+++ assets/utils.js
@@ -1,6 +1,26 @@
+// =========================================================================
+// ULTILITIES FOR FORMATTING AND CONVERSION CLIMATIC DATA
+// =========================================================================
+
+// Format current data
 export const formatDate = (date) => {
     const options = {weekday: 'long', day: 'numeric', month: 'long'};
     const formatter = new Intl.DateTimeFormat('pt-BR', options);
     
+    //Formats and capitalizes only the first letter of the string.
     return formatter.format(date).replace(/^\w/, (c) => c.toUpperCase());
+};
+
+// Converts Celsius to Fahrenheit with 1 decimal place accuracy.
+export const celsiusToFahrenheit = (celsius) => {
+    const fahrenheit = (celsius * 9/5) + 32;
+
+    return parseFloat(fahrenheit.toFixed(1));
+};
+
+// Converts Fahrenheit to Celsius with 1 decimal place accuracy.
+export const fahrenheitToCelsius = (fahrenheit) => {
+    const celsius = (fahrenheit - 32) * 5/9;
+
+    return parseFloat(celsius.toFixed(1));
 };
