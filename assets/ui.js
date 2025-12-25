b699f05c769f5105963a2f3606dbdda5f3a9212d
Rodrigo Brito <rodrigobritocontato@gmail.com>
2025-12-25 10:26:20 -0400

    feat(ui): implement updateMainWeather function

--- assets/ui.js
+++ assets/ui.js
@@ -16,3 +16,15 @@ const windEl = document.getElementById('wind');
 const pressureEl = document.getElementById('pressure');
 const feelsLikeEl = document.getElementById('feels-like');
 const forecastContainerEl = document.getElementById('forecast-container');
+
+
+export const updateMainWeather = (data) => {
+    cityNameEl.textContent = data.name;
+    currentDateEl.textContent = formatDate(new Date());
+    tempMainEl.textContent = `${Math.round(data.main.temp)}°`;
+    weatherDescEl.textContent = translateWeatherDescription(data.weather[0].description);
+
+    const iconUrl = getWeatherIconUrl(data.weather[0].icon, '4x');
+    weatherIconLargeEl.innerHTML = `<img src="${iconUrl}" alt="Clima" class="w-full">`;
+};
+
